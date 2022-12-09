49e2bbcd569283347c5d3426144d3a3c4665f86b
serratusakiphat <devthree@scipiomundine.com>
2022-12-08 23:29:11 -0800

    object keys updated - typos

--- asg_backend/dbModels/fitnessModel.js
+++ asg_backend/dbModels/fitnessModel.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const fitnessSchema = new Schema ({
     username: { type: String, required: true },
     description: { type: String, required: true },
-    description: { type: Number, required: true },
-    description: { type: Date, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
 }, {
     timestamps: true,
 });

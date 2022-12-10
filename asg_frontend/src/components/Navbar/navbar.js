6abd2bdf144e5f5dc6a2fe6be37785a503300425
serratusakiphat <devthree@scipiomundine.com>
2022-12-10 05:02:31 -0800

    navbar items

--- asg_frontend/src/components/Navbar/navbar.js
+++ asg_frontend/src/components/Navbar/navbar.js
@@ -1,18 +1,19 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../Navbar/navstyles.css';
 import * as Icons from "react-icons/fa";
 import ASG from '../../asgLogo.png';
+import { navItems } from './navItems';
 
 function Navbar() {
   const [mobile, setMobile] = useState(false);
-  const [sidebar, setSideBar] = useState(false);
+  const [sidebar, setSidebar] = useState(false);
 
   useEffect(() => {
     if(window.innerWidth < 1065) {
       setMobile(true);
     }
-  })
+  }, [])
 
   useEffect(() => {
     const handleSize = () => {
@@ -20,6 +21,7 @@ function Navbar() {
         setMobile(true);
       } else {
         setMobile(false);
+        setSidebar(false);
       }
     };
 
@@ -32,57 +34,60 @@ function Navbar() {
   return (
     <>
       <nav className="navbar">
-        <Link to="/" className="navbar-logo">
+        <Link to="/" className="navbar-logo" onClick={() => setSidebar(false)}>
           <img src={ASG} alt="ASG" />
           {/* I do not own any rights to this image - content credited to https://asginc.us/ */}
         </Link>
         {!mobile && (
           <ul className="nav-items">
-            <li className="nav-item">
-              <Link to="/" className="nav-link"> {<Icons.FaDumbbell />}
-                <span>Workouts</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/create" className="nav-link">{<Icons.FaHeart />}
-                <span>Create Fitness Log</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/user" className="nav-link">{<Icons.FaUser />}
-                <span>New User</span>
-              </Link>
-            </li>
+            {navItems.map((item) => {
+              return (
+                <li key={item.id} className={item.nName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
           </ul>
         )}
         {!mobile}
 
         {mobile && (
           <div className="sidebar-full">
-            {sidebar ? <Icons.FaTimes className="sidebar-click" onClick={() => setSideBar(!sidebar)} /> : (<Icons.FaBars className="sidebar-click" onClick={() => setSideBar(!sidebar)} />)}
+            {sidebar ? (
+              <Icons.FaTimes
+                className="sidebar-click"
+                onClick={() => setSidebar(!sidebar)}
+              />
+            ) : (
+              <Icons.FaBars
+                className="sidebar-click"
+                onClick={() => setSidebar(!sidebar)}
+              />
+            )}
           </div>
         )}
       </nav>
 
       <div className={sidebar ? "sidebar active" : "sidebar"}>
         <ul className="sidebar-items">
-          <li className="sidebar-item">
-            <Link to="/" className="sidebar-link">{<Icons.FaDumbbell />}
-              <span>Workouts</span>
-            </Link>
-          </li>
-          <li className="sidebar-item">
-            <Link to="/create" className="sidebar-link">{<Icons.FaHeart />}
-              <span>Create Fitness Log</span>
-            </Link>
-          </li>
-          <li className="sidebar-item">
-            <Link to="/user" className="sidebar-link">{<Icons.FaUser />}
-              <span>New User</span>
-            </Link>
-          </li>
+          {navItems.map((item) => {
+            return (
+              <li
+                key={item.id}
+                className={item.sName}
+                onClick={() => setSidebar(false)}
+              >
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
-
       </div>
     </>
   );
